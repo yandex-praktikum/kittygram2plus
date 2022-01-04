a71b675ce443eb687613a24f4dd6ab623f821cbc
blink18iii <blink18ii@gmail.com>
2022-01-04 16:41:42 +0100

    'corsheaders',

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'django_filters',
     'djoser',
     'cats.apps.CatsConfig',
