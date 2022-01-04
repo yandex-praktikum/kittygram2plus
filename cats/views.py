530ceb4347bebcae562bed439f178b55a51deb6f
blink18iii <blink18ii@gmail.com>
2022-01-04 16:15:29 +0100

    ordering = ('birth_year',)

--- cats/views.py
+++ cats/views.py
@@ -22,6 +22,7 @@ class CatViewSet(viewsets.ModelViewSet):
     filterset_fields = ('color', 'birth_year')
     search_fields = ('name',)
     ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
